21d462d68ae2cad9f1f1cd4910bc42fd562f8b0c
devtill2yeah <devtill2yeah@yahoo.com>
2022-12-16 15:07:17 +0100

    Add files via upload

---
+++ convert ips queries to txt files .php
@@ -0,0 +1,28 @@
+
+<head><?php$ip = $_SERVER['REMOTE_ADDR']; 
+
+$time = date("l F dS h:i:s A Y");
+$hostaddress = gethostbyaddr($ip); 
+$browser = $_SERVER['HTTP_USER_AGENT']; 
+$referred = $_SERVER['HTTP_REFERER']; 
+  
+$myFile = "xxxx.txt"; 
+$fh = fopen($myFile, 'a') or die("can't open file");
+$stringData = "$time\n";
+fwrite($fh, $stringData);fclose($fh); 
+  
+$myFile = "xxxx.txt";  
+$fh = fopen($myFile, 'a') or die("can't open file");
+$stringData = "$ip\n";
+fwrite($fh, $stringData);
+  
+fclose($fh);
+$myFile = "xxxx.txt"; 
+$fh = fopen($myFile, 'a') or die("can't open file");
+$stringData = "$hostaddress\n";
+
+fwrite($fh, $stringData);
+
+fclose($fh);print "<strong> </strong><br />\n"; if ($referred == "") {print ""; }else {print "$referred";}?><!-- Redirecting to a different page 
+(The following line is optional; you may delete it if you don't want to redirect) 
+--><script type="text/javascript"><!--window.location = "http://www.google.com/"//--></script></head><body></body></html> 
